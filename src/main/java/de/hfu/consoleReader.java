f09ad30c7503812860e1ba6b35d83e2ad245caa3
Cedric Casper <cedriccasper@protonmail.ch>
2022-11-24 11:45:00 +0100

    added javadoc

--- src/main/java/de/hfu/consoleReader.java
+++ src/main/java/de/hfu/consoleReader.java
@@ -4,13 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Unglaublich wichtiges Programm für das OSSWE Praktikum 05
+ * @author Cedric Casper
+ * @version 1.0
+ */
 public class consoleReader {
+    /**
+     * Die Hauptklasse - halt dich fest - nimmt einen Input über die
+     * Konsole entgegen und dann - unglaublich! - wird dieser in
+     * Uppercase umgewandelt auf der Konsole ausgegeben.
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        // Enter data using BufferReader
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = reader.readLine();
-
+        String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
         System.out.println(input.toUpperCase());
     }
 }
